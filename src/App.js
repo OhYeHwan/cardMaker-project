1e4b12a9c29e0ee93ed464d95ca35be7a8894c78
OOHYEE <azss0470@gmail.com>
2021-11-02 16:31:37 +0900

    로그인 & 로그아웃 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Login from './components/login/login';
 import styles from './app.module.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Maker from './components/maker/maker';
 
 function App({ authService }) {
   return (
     // 단순히 스타일링만 위한 것이라면 : div
     // 의미있는 그룹이라면 : section, hearder ...
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
+import { useHistory } from 'react-router-dom';
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: '/maker',
+      state: { id: userId },
+    });
+  };
+
   const onLogin = (event) => {
     console.log(event.currentTarget.textContent);
-    authService.login(event.currentTarget.textContent).then(console.log);
+    authService
+      .login(event.currentTarget.textContent)
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    });
+  });
+
   return (
     <section className={styles.login}>
       <Header />
