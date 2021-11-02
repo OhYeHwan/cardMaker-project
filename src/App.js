59b0d36cea288e4d73216ec7ad57f76cb9c8a09e
OOHYEE <azss0470@gmail.com>
2021-11-02 15:08:48 +0900

    add login component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import Login from "./components/login/login";
+import Login from './components/login/login';
+import styles from './app.module.css';
 
 function App({ authService }) {
-  return <Login authService={authService} />;
+  return (
+    // 단순히 스타일링만 위한 것이라면 : div
+    // 의미있는 그룹이라면 : section, hearder ...
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
+  );
 }
 
 export default App;
